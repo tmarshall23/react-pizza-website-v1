f50792407cb681ebc81ee118c5c99a63fbede767
Tamara Marshall <tamaramarshall423@gmail.com>
2022-11-06 23:56:23 -0800

    Completed Code-Along with Build a React Website Using Styled Components. TM

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { SidebarContainer, Icon, CloseIcon, SidebarMenu, SidebarLink, SideBtnWrap,
+     SidebarRoute
+    } from './SidebarElements';
+
+
+const Sidebar = ({isOpen, toggle}) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+        <Icon onClick={toggle}>
+            <CloseIcon/>
+        </Icon>
+        <SidebarMenu>
+            <SidebarLink to="/">Pizza</SidebarLink>
+            <SidebarLink to="/">Dessert</SidebarLink>
+            <SidebarLink to="/">Full Menu</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+            <SidebarRoute to="/">Order Now</SidebarRoute>
+        </SideBtnWrap>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar
